c1d5d0f03f2777773a4d3851486fad87fdfab7c0
CarloZ <carloz190405@gmail.com>
2025-12-10 19:47:41 -0600

    se creo un archivo para los recusos

---
+++ recursos.py
@@ -0,0 +1,22 @@
+from flask_restful import Resource
+
+from flask import make_response, render_template
+# Los recursos que mi servidor tendra disponible
+
+
+
+# VAmos a crear Recursos - Para nosotros poder crear recursos
+# lo haremos a traves de clases y objetos
+class HolaMundo(Resource):
+    # Los recursos van a poder ejecutar dos acciones -> metodos
+    def get(self):
+        return {'hola': 'mundo'}
+
+# Sera un recurso
+class PantallaInicio(Resource):
+    def get(self):
+        #Renderizamos el contenido del archivo html
+        contenido = render_template('index.html')
+
+        # Retornar el contenido renderizado en una respuesta
+        return make_response(contenido)
